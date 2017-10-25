ddcaae22fc3ae552a11d2af68fba493b73464c53
KevinT <kevin.tai.mail@gmail.com>
2017-10-24 20:59:07 -0700

    NSString

--- Strings/main.m
+++ Strings/main.m
@@ -10,10 +10,18 @@
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-    NSString *name = @"Kevin";
+    NSString *name = @"#Kevin";
     NSLog(@"name variable is %@", name);
     NSUInteger nameLength = name.length;
     NSLog(@"%@ length is %lu", name, nameLength);
+    NSString *bootcamp = @"Lighthouse Labs";
+    NSString *bootcampUC = bootcamp.uppercaseString;
+    NSString *bootcampLC = bootcamp.lowercaseString;
+    NSString *bootcampAppend = [bootcamp stringByAppendingString:name];
+    NSString *replacementString = @"LL";
+    NSString *bootcampReplace = [bootcamp stringByReplacingOccurrencesOfString:bootcamp withString:replacementString];
+    NSLog(@"UC: %@ | LC: %@", bootcampUC, bootcampLC);
+    NSLog(@"Append: %@ | Replace: %@", bootcampAppend, bootcampReplace);
   }
   return 0;
 }
