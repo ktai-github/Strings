55dc5eaccc574793c3cacc4800f50a04c4abfb1b
KevinT <kevin.tai.mail@gmail.com>
2017-10-24 20:18:44 -0700

    first commit

--- Strings/main.m
+++ Strings/main.m
@@ -9,9 +9,9 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+  @autoreleasepool {
+    NSString *name = @"Kevin";
+    NSLog(@"name variable is %@", name);
+  }
+  return 0;
 }
